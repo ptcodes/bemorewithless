b886fec250e656c268b6f7966d0df1488d39cb9a
Pavel Timofeev <pav.timofeev@gmail.com>
2013-10-05 16:04:34 -0300

    Change Amazon AWS host bucket

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -12,7 +12,7 @@ elsif Rails.env.production?
       provider:              'AWS',
       aws_access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region:                'sa-east-1'
+      region:                's3-eu-west-1'
     }
     config.fog_directory  = 'bemorewithless'                     # required
     config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
