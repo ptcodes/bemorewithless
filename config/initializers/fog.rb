e7029bebff6ff76607a447f9e32724e4e43d20a3
Konstantin.Portnov <x0000ff@gmail.com>
2013-10-10 15:58:39 -0300

    ADD: CarrierWave returns full url on test & development

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -2,6 +2,7 @@ if Rails.env.test? || Rails.env.development?
   CarrierWave.configure do |config|
     config.storage = :file
     config.enable_processing = false
+    config.asset_host = 'http://localhost:3000'
   end
 elsif Rails.env.production?
   CarrierWave.configure do |config|
