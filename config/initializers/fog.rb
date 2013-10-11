7452d10b64f88b63550cbfa9a79b3149d4a92933
Konstantin.Portnov <x0000ff@gmail.com>
2013-10-10 21:53:33 -0300

    REF: AWS directory is moving to ENV

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -15,7 +15,7 @@ elsif Rails.env.production?
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
       region:                'us-east-1'
     }
-    config.fog_directory  = 'bemorewithless'                     # required
+    config.fog_directory  = ENV['AWS_DIRECTORY']                     # required
     config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
   end
 end
